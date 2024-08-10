5e42bfff3935ee5cb45e73261ad0a1495a745014
ooookai <ooookai@gmail.com>
2024-08-11 04:38:25 +0800

    init: setup eslint
    with `@antfu/eslint-config`

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+console.log('root config')
+
+export default antfu(
+  {
+    formatters: true,
+    rules: {
+      'no-console': 'off',
+    },
+  },
+).renamePlugins({
+  // Doc: https://github.com/antfu/eslint-config?tab=readme-ov-file#plugins-renaming
+  import: 'import-antfu', // It seems there is a conflict with the config from the nuxt-ui package.
+})
