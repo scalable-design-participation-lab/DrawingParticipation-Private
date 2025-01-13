dd749ad6525b68b92966a5dda1f40c2e3b255f37
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-13 10:37:33 -0500

    feat: adding vitest workspace and configuration

---
+++ apps/restart-ukraine/vitest.config.mjs
@@ -0,0 +1,19 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '#ui': path.resolve(
+        __dirname,
+        './node_modules/@nuxt/ui/dist/runtime/ui.mjs',
+      ),
+    },
+  },
+})
