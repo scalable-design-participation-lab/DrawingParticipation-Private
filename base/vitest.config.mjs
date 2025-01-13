5daf2a99644aea11d49a57f90743863f97400765
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-13 12:26:21 -0500

    migration: legacy test case & vitest configuration & documentation

--- base/vitest.config.mjs
+++ base/vitest.config.mjs
@@ -4,12 +4,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
   },
   resolve: {
     alias: {
+      '@components': '/app/components',
       '#ui': path.resolve(
         __dirname,
         './node_modules/@nuxt/ui/dist/runtime/ui.mjs',
