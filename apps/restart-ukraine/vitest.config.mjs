84ee66e23694b3bfeef44a35df627c8ef99834c4
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-13 15:46:22 -0500

    test: migrating tests & add README for pinia bug

--- apps/restart-ukraine/vitest.config.mjs
+++ apps/restart-ukraine/vitest.config.mjs
@@ -4,12 +4,15 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
   },
   resolve: {
     alias: {
+      '@base': path.resolve(__dirname, '../../base/app'),
+      '@components': '/app/components',
       '#ui': path.resolve(
         __dirname,
         './node_modules/@nuxt/ui/dist/runtime/ui.mjs',
