afb3575ca00f468a95e45cd5dc1ede998e965a68
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-16 12:36:00 -0500

    fix: ui bug fix with map type

--- apps/restart-ukraine/app/pages/map.vue
+++ apps/restart-ukraine/app/pages/map.vue
@@ -33,7 +33,6 @@ import { useFirestore } from 'vuefire'
 import { collection, getDocs } from 'firebase/firestore'
 import { useMapStore } from '@base/stores/map'
 import { useFeatureStore } from '@base/stores/features'
-
 const featureStore = useFeatureStore()
 const mapStore = useMapStore()
 const db = useFirestore()
@@ -65,18 +64,18 @@ const leftItems = ref([
 ])
 
 // Add right items for map controls
-const currentMapType = ref('light')
+const {mapType} = storeToRefs(mapStore)
 const rightItems = ref([
   {
     icon: computed(() =>
-      currentMapType.value === 'light'
+      mapType.value === 'vector'
         ? 'i-heroicons:map'
         : 'i-heroicons:globe-americas-20-solid',
     ),
     onClick: () => {
-      currentMapType.value =
-        currentMapType.value === 'light' ? 'satellite' : 'light'
-      mapStore.setMapType(currentMapType.value)
+      mapType.value =
+        mapType.value === 'vector' ? 'satellite' : 'vector'
+      mapStore.setMapType(mapType.value)
     },
   },
 ])
