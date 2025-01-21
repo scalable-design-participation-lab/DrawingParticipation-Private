434b367e7c2f47d5484ca2f3edc19b2c268330a5
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-21 12:44:21 -0500

    feat: setting up datastructure for categorizing heatmap

--- apps/restart-ukraine/app/pages/result.vue
+++ apps/restart-ukraine/app/pages/result.vue
@@ -26,13 +26,10 @@
 
 
 <script setup lang="ts">
-import { onMounted, ref, computed, watch } from 'vue'
+import {  ref, computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import MapIntroModal from '../components/MapIntroModal.vue'
-import { useFirestore } from 'vuefire'
-import { collection, getDocs } from 'firebase/firestore'
 import { useMapStore } from '@base/stores/map'
-import { useAllFeatureStore } from '../stores/all-features'
 
 const mapStore = useMapStore()
 const route = useRoute()
