917ffa3e81aee3c4dd3746ccf194bec2315c85d1
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-22 11:05:35 -0500

    feat: integrate layer with generalized map

--- apps/restart-ukraine/app/pages/heatmap.vue
+++ apps/restart-ukraine/app/pages/heatmap.vue
@@ -3,15 +3,14 @@ import { ref, computed } from "vue";
 import { useAllFeatureStore } from "../stores/all-features";
 import GeoJSON from "ol/format/GeoJSON";
 import type { Category} from "../stores/types/store";
+import GeneralizedBackgroundMap from "@base/components/GeneralizedBackgroundMap.vue";
+
+const blur = useState("blur", () => 20);
+const radius = useState("radius", () => 20);
+const filterOption = useState<Category | string>("filterOption", () => "");
+const geoJson = new GeoJSON();
 
 const { allFeatures, featuresByCategory } = useAllFeatureStore(); 
-const center = ref([3172858.2941718884, 6317486.347640147]);
-const projection = ref("EPSG:3857");
-const zoom = ref(5);
-const blur = ref(20);
-const radius = ref(20);
-const geoJson = new GeoJSON();
-const filterOption = ref<Category | string>("")
 const filterOptions = computed(() => Object.keys(featuresByCategory));
 const geoJsonFeatures = computed(() => {
   // Filter features based on the selected filter option 
@@ -47,80 +46,34 @@ function featuresloadend() {
 }
 </script>
 
-  
-
 <template>
-    <form class="absolute z-50 bg-white shadow-lg p-4 text-black">
-      <fieldset>
-        <label for="blur">Blur</label>
-        <input
-          type="range"
-          id="blur"
-          min="0"
-          max="100"
-          step="1"
-          v-model.number="blur"
-        />
-        <span class="description">{{ blur }}</span>
-      </fieldset>
-      <fieldset>
-        <label for="radius">Radius</label>
-        <input
-          type="range"
-          id="radius"
-          min="0"
-          max="100"
-          step="1"
-          v-model.number="radius"
-        />
-        <span class="description">{{ radius }}</span>
-      </fieldset>
-      <fieldset>
-        <legend>Filter Options</legend>
-        <div v-for="option in filterOptions" :key="option" class="mb-2 flex items-center space-x-2">
-          <input
-            type="radio"
-            :id="`filter-${option}`"
-            :value="option"
-            v-model="filterOption"
-          />
-          <label :for="`filter-${option}`"> {{ option }}</label>
-        </div>
+  <form class="absolute z-50 bg-white shadow-lg p-4 text-black">
+    <fieldset>
+      <label for="blur">Blur</label>
+      <input type="range" id="blur" min="0" max="100" step="1" v-model.number="blur" />
+      <span class="description">{{ blur }}</span>
     </fieldset>
-    </form>
-  
-    <ol-map
-      ref="map"
-      :loadTilesWhileAnimating="true"
-      :loadTilesWhileInteracting="true"
-      style="height: 100vh"
-    >
-      <ol-view
-        ref="view"
-        :center="center"
-        :zoom="zoom"
-        :projection="projection"
-      />
-  
-      <ol-tile-layer>
-        <ol-source-osm />
-      </ol-tile-layer>
-  
-      <ol-heatmap-layer
-        title="heatmap"
-        :blur="blur"
-        :radius="radius"
-        :zIndex="1"
-      >
-        <ol-source-vector
-        :features="geoJsonFeatures"
-        :format="geoJson"
-        @featuresloadstart="featuresloadstart"
-        @featuresloadend="featuresloadend"
-        @featuresloaderror="featuresloaderror"
-        >
+    <fieldset>
+      <label for="radius">Radius</label>
+      <input type="range" id="radius" min="0" max="100" step="1" v-model.number="radius" />
+      <span class="description">{{ radius }}</span>
+    </fieldset>
+    <fieldset>
+      <legend>Filter Options</legend>
+      <div v-for="option in filterOptions" :key="option" class="mb-2 flex items-center space-x-2">
+        <input type="radio" :id="`filter-${option}`" :value="option" v-model="filterOption" />
+        <label :for="`filter-${option}`"> {{ option }}</label>
+      </div>
+    </fieldset>
+  </form>
+
+  <GeneralizedBackgroundMap ref="baseMap">
+    <template #layers>
+      <ol-heatmap-layer title="heatmap" :blur="blur" :radius="radius" :zIndex="1">
+        <ol-source-vector :features="geoJsonFeatures" :format="geoJson" @featuresloadstart="featuresloadstart"
+          @featuresloadend="featuresloadend" @featuresloaderror="featuresloaderror">
         </ol-source-vector>
       </ol-heatmap-layer>
-    </ol-map>
-  </template>
-  
+    </template>
+  </GeneralizedBackgroundMap>
+</template>
--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import path from 'node:path'
 
 export default defineNuxtConfig({
   extends: [
