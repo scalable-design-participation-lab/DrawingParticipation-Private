1fe2f690d9071af1b8235d661a0bc629d9f7bc0d
Khoi Ngo <ngo.kho@northeastern.edu>
2025-01-22 13:32:37 -0500

    feat: componenize heatmap

---
+++ base/app/components/GeoSpatialLayer/HeatMap.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import type Feature from 'ol/Feature'
+import GeoJSON from 'ol/format/GeoJSON'
+import type { Geometry } from 'ol/geom'
+
+/**
+ * Type definition for the props of the HeatmapLayer component.
+ */
+export interface HeatmapLayerProps {
+  /**
+   * The amount of blur applied to the heatmap.
+   * Larger values result in smoother heatmaps.
+   * @default 20
+   */
+  blur?: number
+
+  /**
+   * The radius of influence for each data point in the heatmap.
+   * @default 20
+   */
+  radius?: number
+
+  /**
+   * The format used to read features.
+   * Defaults to the GeoJSON format from OpenLayers.
+   * @default new GeoJSON()
+   */
+  format?: GeoJSON
+
+  /**
+   * An array of OpenLayers `Feature` objects that define the data points to be visualized in the heatmap.
+   * @default []
+   */
+  features?: Feature<Geometry>[]
+
+  /**
+   * Callback function triggered when feature loading starts.
+   * Logs "features load start" by default.
+   * @default () => console.log('features load start')
+   */
+  featuresloadstart?: (event?: Event) => void
+
+  /**
+   * Callback function triggered when feature loading ends.
+   * Logs "features load end" by default.
+   * @default () => console.log('features load end')
+   */
+  featuresloadend?: (event?: Event) => void
+
+  /**
+   * Callback function triggered when an error occurs during feature loading.
+   * Logs "features load error" by default.
+   * @default () => console.log('features load error')
+   */
+  featuresloaderror?: (event?: Event) => void
+}
+
+/**
+ * Define props with default values.
+ */
+withDefaults(defineProps<HeatmapLayerProps>(), {
+  blur: 20,
+  radius: 20,
+  format: () => new GeoJSON(),
+  features: () => [],
+  featuresloadstart: () => console.log('features load start'),
+  featuresloadend: () => console.log('features load end'),
+  featuresloaderror: () => console.log('features load error'),
+})
+</script>
+
+<template>
+  <ol-heatmap-layer title="heatmap" :blur="blur" :radius="radius" :z-index="1">
+    <ol-source-vector
+      :features="features"
+      :format="format"
+      @featuresloadstart="featuresloadstart"
+      @featuresloadend="featuresloadend"
+      @featuresloaderror="featuresloaderror"
+    />
+  </ol-heatmap-layer>
+</template>
