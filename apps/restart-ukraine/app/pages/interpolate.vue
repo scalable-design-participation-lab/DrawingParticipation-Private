803544f291b8fe56139af4feae72efc2eac37036
Khoi Ngo <ngo.kho@northeastern.edu>
2025-02-10 12:42:15 -0500

    feat: interpolate component

--- apps/restart-ukraine/app/pages/interpolate.vue
+++ apps/restart-ukraine/app/pages/interpolate.vue
@@ -1,32 +1,18 @@
 <script setup lang="ts"> 
 import GeneralizedBackgroundMap from "@base/components/GeneralizedBackgroundMap.vue";
+import Interpolate from "@base/components/GeoSpatialLayer/Interpolate.vue";
 import * as turf from "@turf/turf"
-import GeoJSON from "ol/format/GeoJSON";
-var points = turf.randomPoint(30, { bbox: [50, 30, 70, 50] });
-
-// add a random property to each point
+var points = turf.randomPoint(100, { bbox: [50, 30, 70, 50] });
 turf.featureEach(points, function (point) {
   point.properties.solRad = Math.random() * 50;
 });
-var options = { gridType: "points", property: "solRad", units: "miles" };
-var grid = turf.interpolate(points, 100, options);
-
-
-// convert to geojson
-const geoJson = new GeoJSON();
-const gridFeatures = geoJson.readFeatures(grid, {
-    dataProjection: "EPSG:4326",
-    featureProjection: "EPSG:3857",
-})
 </script>
 
 
 <template> 
 <GeneralizedBackgroundMap ref="baseMap" :min-zoom="0">
     <template #layers> 
-        <ol-vector-layer id="layerId" :z-index="1" :visible="true" >
-            <ol-source-vector :features="gridFeatures" />
-        </ol-vector-layer>
+        <Interpolate :points="points" property="solRad" :gridType="'points'" :units="'miles'" :visible="true" :zIndex="1" />
     </template>
 </GeneralizedBackgroundMap>
 </template>
---
+++ base/app/components/GeoSpatialLayer/Interpolate.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import * as turf from '@turf/turf'
+import GeoJSON from 'ol/format/GeoJSON'
+
+/**
+ * Props for configuring interpolation.
+ */
+interface InterpolationProps {
+  /**
+   * A collection of input points as a Turf.js FeatureCollection.
+   * Must include a property for interpolation.
+   */
+  points: any
+
+  /**
+   * The name of the property to interpolate.
+   */
+  property: string
+
+  /**
+   * The number of grid points to generate.
+   */
+  gridPoints?: number
+
+  /**
+   * The type of grid for interpolation (default is "points").
+   */
+  gridType?: 'points' | 'hex' | 'square' | 'triangle'
+
+  /**
+   * The measurement units for grid spacing.
+   */
+  units?: 'miles' | 'kilometers'
+
+  /**
+   * Visibility of the interpolation layer.
+   */
+  visible?: boolean
+
+  /**
+   * The z-index of the layer.
+   */
+  zIndex?: number
+}
+
+const props = withDefaults(defineProps<InterpolationProps>(), {
+  gridPoints: 100,
+  gridType: 'points',
+  units: 'miles',
+  visible: true,
+  zIndex: 1,
+})
+
+const gridFeatures = computed(() => {
+  if (!props.points)
+    return []
+
+  const options = {
+    gridType: props.gridType,
+    property: props.property,
+    units: props.units,
+  }
+
+  const interpolatedGrid = turf.interpolate(props.points, props.gridPoints, options)
+  const geoJson = new GeoJSON()
+
+  return geoJson.readFeatures(interpolatedGrid, {
+    dataProjection: 'EPSG:4326',
+    featureProjection: 'EPSG:3857',
+  })
+})
+</script>
+
+<template>
+  <ol-vector-layer :z-index="zIndex" :visible="visible">
+    <ol-source-vector :features="gridFeatures" />
+  </ol-vector-layer>
+</template>
