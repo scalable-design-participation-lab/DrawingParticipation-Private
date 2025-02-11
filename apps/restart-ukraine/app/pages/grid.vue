cb67ea68565d7096a92bc20fd4c7b8697f1b7ba6
Khoi Ngo <ngo.kho@northeastern.edu>
2025-02-11 14:50:33 -0500

    feat: component GridController

--- apps/restart-ukraine/app/pages/grid.vue
+++ apps/restart-ukraine/app/pages/grid.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
 import { useAllFeatureStore } from '@base/stores/all-features';
-import Grid, { type PointFeature } from '@base/components/GeoSpatialLayer/Grid.vue';
+import Grid, { type GridType, type PointFeature } from '@base/components/GeoSpatialLayer/Grid.vue';
+import GridController from '@base/components/GeoSpatialLayer/GridController.vue';
 
 const { featuresByType } = useAllFeatureStore();
+const shape = ref<GridType>('Hexagon');
+const baseHue = ref<number>(0);
+const cellSide = ref<number>(0.2);
+const visible = ref<boolean>(true); 
+
 </script>
 <template>
+  <GridController
+   v-model:baseHue="baseHue"
+   v-model:cellSide="cellSide"
+   v-model:visible="visible"
+   v-model:shape="shape" /> 
+
   <GeneralizedBackgroundMap>
     <template #layers>
       <Grid 
       :points="featuresByType.Point as PointFeature[]" 
       :bbox="[28.462271, 49.215576, 28.570271, 49.265576]"
-      :cellSide="0.2"
+      :cellSide="cellSide"
       layerId="hexLayer"
-      :baseHue="250" 
-      shape="Hexagon"
-      :visible="true"/>
+      :baseHue="baseHue" 
+      :shape="shape"
+      :visible="visible"/>
     </template>
   </GeneralizedBackgroundMap>
 </template>
