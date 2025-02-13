4a70547cc345dcb879e75ecafb016418b5a89fe3
Khoi Ngo <ngo.kho@northeastern.edu>
2025-02-13 13:34:56 -0500

    refractor: normalize interface for components

--- apps/restart-ukraine/app/pages/grid.vue
+++ apps/restart-ukraine/app/pages/grid.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { useAllFeatureStore } from '@base/stores/all-features';
-import Grid, { type GridType, type PointFeature } from '@base/components/GeoSpatialLayer/Grid/Grid.vue';
+import * as turf from '@turf/turf'; 
+import Grid, { type GridType } from '@base/components/GeoSpatialLayer/Grid/Grid.vue';
 import GridController from '@base/components/GeoSpatialLayer/Grid/GridController.vue';
+import GeoJSON from 'ol/format/GeoJSON'
+import { toLonLat } from 'ol/proj';
 
 const { featuresByType } = useAllFeatureStore();
+const geoJson = new GeoJSON();
+const pointFeatures = computed(() => featuresByType.Point);
+const dataPoints = computed(() => {
+  const points = pointFeatures.value.map(({ coordinates }) => {
+    return turf.point(toLonLat(coordinates as [number, number]));
+  });
+  return turf.featureCollection(points);
+})
+const features = computed(() => {
+  return geoJson.readFeatures(dataPoints.value, {
+    dataProjection: 'EPSG:4326',
+    featureProjection: 'EPSG:3857',
+  });
+});
 const shape = ref<GridType>('Hexagon');
 const baseHue = ref<number>(0);
 const cellSide = ref<number>(0.2);
@@ -22,7 +39,7 @@ const width = ref<number>(1);
   <GeneralizedBackgroundMap>
     <template #layers>
       <Grid 
-      :points="featuresByType.Point as PointFeature[]" 
+      :features="features" 
       :bbox="[28.462271, 49.215576, 28.570271, 49.265576]"
       :cellSide="cellSide"
       layerId="hexLayer"
