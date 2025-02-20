cb39bf8e5f73d7bbc51d4356995cae6accb3cbac
Khoi Ngo <ngo.kho@northeastern.edu>
2025-02-20 16:09:49 -0500

    feat: adding simple controller

--- apps/restart-ukraine/app/pages/voronoi.vue
+++ apps/restart-ukraine/app/pages/voronoi.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Voronoi from "@base/components/GeoSpatialLayer/Voronoi/Voronoi.vue";
+import VoronoiController from "@base/components/GeoSpatialLayer/Voronoi/VoronoiController.vue";
 import { useAllFeatureStore } from "@base/stores/all-features";
 import { toLonLat } from "ol/proj";
 
@@ -7,13 +8,17 @@ const {featuresByType} = useAllFeatureStore();
 const coordinates = computed(() => {
     return featuresByType.Point.map(feature => toLonLat(feature.coordinates as [number, number])) as [number, number][];
 })
+const visible = ref<boolean>(true);
+const zIndex = ref<number>(0);
 
 </script>
 
 <template>
-  <GeneralizedBackgroundMap ref="baseMap" :min-zoom="0">
+  <VoronoiController v-model:visible="visible" v-model:zIndex="zIndex" />
+  <GeneralizedBackgroundMap ref="baseMap">
     <template #layers>
-        <Voronoi :coordinates="coordinates" :visible="true" />
+        <Voronoi 
+        :coordinates="coordinates" :visible="visible" :z-index="zIndex" />
     </template>
   </GeneralizedBackgroundMap>
 </template>
---
+++ base/app/components/GeoSpatialLayer/Voronoi/VoronoiController.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const visible = defineModel('visible', { type: Boolean, required: true })
+const zIndex = defineModel('zIndex', { type: Number, required: true })
+</script>
+
+<template>
+  <UCard class="fixed right-6 top-24 w-96 md:w-80 max-h-[calc(100vh-11rem)] z-40 shadow-xl dark:bg-black flex flex-col overflow-hidden">
+    <!-- Visible -->
+    <div class="flex justify-between my-2">
+      <span class="my-1 capitalize text-black font-semibold dark:text-white"> Visibility </span>
+      <UToggle
+        v-model="visible"
+        class="my-1"
+        color="green"
+        size="lg"
+        on-icon="i-heroicons-check-20-solid"
+        off-icon="i-heroicons-x-mark-20-solid"
+      />
+    </div>
+
+    <!-- Z Index Input -->
+    <div class="flex items-center justify-between">
+      <span class="block py-2 capitalize text-black font-semibold dark:text-white">Z-Index</span>
+      <UInput
+        id="z-index"
+        v-model.number="zIndex"
+        type="number"
+        class="w-20 px-1 "
+      />
+    </div>
+  </UCard>
+</template>
