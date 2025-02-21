a04ffb348058582c942b66f7e062fea77c3270bd
Khoi Ngo <ngo.kho@northeastern.edu>
2025-02-21 15:35:57 -0500

    feat: adding value to interpolates

--- apps/restart-ukraine/app/pages/interpolate.vue
+++ apps/restart-ukraine/app/pages/interpolate.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts"> 
 import GeneralizedBackgroundMap from "@base/components/GeneralizedBackgroundMap.vue";
 import Interpolate from "@base/components/GeoSpatialLayer/Interpolate/Interpolate.vue";
-import * as turf from "@turf/turf"
 const bbox = [-180, -90, 180, 90]
-const points: [number, number][] = [
-  [0,0],
-  [0, 45],
-  [0, 90],
-  [45, 0],
-  [45, 45],
-  [45, 90],
-  [90, 0],
-  [90, 45],
-  [90, 90]
-]
+const points: { coordinates: [number, number]; value: number }[] = [
+  { coordinates: [0, 0], value: Math.random() * 100 },
+  { coordinates: [0, 45], value: Math.random() * 100 },
+  { coordinates: [0, 90], value: Math.random() * 100 },
+  { coordinates: [45, 0], value: Math.random() * 100 },
+  { coordinates: [45, 45], value: Math.random() * 100 },
+  { coordinates: [45, 90], value: Math.random() * 100 },
+  { coordinates: [90, 0], value: Math.random() * 100 },
+  { coordinates: [90, 45], value: Math.random() * 100 },
+  { coordinates: [90, 90], value: Math.random() * 100 },
+];
 </script>
 
 
