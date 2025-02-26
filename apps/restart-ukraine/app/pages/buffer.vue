6382061b7233952af2515b54ddf524ed80590a1f
Khoi Ngo <ngo.kho@northeastern.edu>
2025-02-26 16:28:20 -0500

    feat & test: updating buffer interface to take in a FeatureCollection

--- apps/restart-ukraine/app/pages/buffer.vue
+++ apps/restart-ukraine/app/pages/buffer.vue
@@ -3,33 +3,48 @@ import Buffer from '@base/components/GeoSpatialLayer/Buffer/Buffer.vue';
 import BufferController from '@base/components/GeoSpatialLayer/Buffer/BufferController.vue';
 import { useAllFeatureStore } from '@base/stores/all-features';
 import { toLonLat } from 'ol/proj';
-const {featuresByType} = useAllFeatureStore();
+import * as turf from "@turf/turf"
+import { computed, ref } from 'vue';
+
+const { featuresByType } = useAllFeatureStore();
+
 const coordinates = computed(() => {
-    return featuresByType.Point.map(feature => toLonLat(feature.coordinates as [number, number])) as [number, number][];
-})
+    if (!featuresByType.Point || !Array.isArray(featuresByType.Point)) {
+        console.error("Invalid feature data:", featuresByType.Point);
+        return turf.featureCollection([]);
+    }
+    const points = featuresByType.Point.map(feature => {
+        const lonLat = toLonLat(feature.coordinates as [number, number]);
+        return turf.point(lonLat);
+    })
+
+    return turf.featureCollection(points);
+});
+
 const visible = ref(true);
 const zIndex = ref(1);
 const radius = ref(200);
 const mode = ref<'none' | 'union' | 'intersect'>('none');
 const units = ref<'meters' | 'kilometers' | 'miles'>('meters');
 </script>
+
 <template>
     <BufferController
-    v-model:visible="visible"
-    v-model:zIndex="zIndex"
-    v-model:radius="radius"
-    v-model:mode="mode"
-    v-model:units="units"
+        v-model:visible="visible"
+        v-model:zIndex="zIndex"
+        v-model:radius="radius"
+        v-model:mode="mode"
+        v-model:units="units"
     />
     <GeneralizedBackgroundMap ref="baseMap" :min-zoom="0"> 
         <template #layers> 
             <Buffer
-            :units="units"
-            :mode="mode"
-            :coordinates="coordinates"
-            :visible="visible"
-            :z-index="zIndex"
-            :radius="radius"
+                :units="units"
+                :mode="mode"
+                :coordinates="coordinates"
+                :visible="visible"
+                :z-index="zIndex"
+                :radius="radius"
             />
         </template>
     </GeneralizedBackgroundMap>
