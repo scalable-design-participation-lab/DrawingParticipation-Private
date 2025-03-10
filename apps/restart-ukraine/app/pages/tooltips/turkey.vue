89085277a5c2da2a521884a1e257abbd24d5c2e9
Khoi Ngo <ngo.kho@northeastern.edu>
2025-03-10 13:14:50 -0400

    feat: adding more example

---
+++ apps/restart-ukraine/app/pages/tooltips/turkey.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { toLonLat, transform } from 'ol/proj'
+import {ref, onMounted } from 'vue'
+import GeoJSON from 'ol/format/GeoJSON'
+import { click, pointerMove } from 'ol/events/condition'
+import ToolTips from '@base/components/InteractiveLayer/ToolTips.vue'
+import { useDataStore } from '@base/stores/data'
+import * as turf from '@turf/turf'
+import { useAllFeatureStore } from '@base/stores/all-features'
+
+// References and reactive state
+// Define a ref to hold the reference to the child component
+const mapRef = ref(null)
+
+// Use a computed prop to access the exposed `mapInstance` from the child component
+const mapInstance = computed(() => {
+  return mapRef.value ? mapRef.value.mapInstance : null
+}) 
+const dataStore = useDataStore()
+await dataStore.fetchData('https://raw.githubusercontent.com/alpers/Turkey-Maps-GeoJSON/master/tr-cities-airports.json')
+
+const data = dataStore.data 
+const pointStyle = {
+    radius: 6,
+    fill: 'rgba(0, 100, 255, 0.8)',
+    stroke: {
+      color: 'white',
+      width: 2,
+    },
+  }
+const features = computed(() => {
+  const geoJson = new GeoJSON()
+  return geoJson.readFeatures(data, {
+    dataProjection: 'EPSG:4326',
+    featureProjection: 'EPSG:3857',
+  })
+})
+</script>
+
+<template>
+  <GeneralizedBackgroundMap ref="mapRef" :min-zoom="0">
+    <template #layers> 
+          <ToolTips :mapInstance="mapInstance" >
+            <!-- Vector Layer with Interactions -->
+            <ol-vector-layer>
+              <ol-source-vector
+                :features="features"
+                :format="new GeoJSON()"
+              />
+              <!-- Default Style -->
+              <ol-style>
+                <ol-style-fill color="rgba(0, 0, 0, 0)" />
+                <ol-style-stroke color="green" :width="10" />
+                <ol-style-circle :radius="pointStyle.radius">
+                  <ol-style-fill :color="pointStyle.fill" />
+                  <ol-style-stroke
+                    :color="pointStyle.stroke.color"
+                    :width="pointStyle.stroke.width"
+                  />
+                </ol-style-circle>
+              </ol-style>
+            </ol-vector-layer>
+          </ToolTips> 
+    </template>
+  </GeneralizedBackgroundMap>
+ 
+</template>
