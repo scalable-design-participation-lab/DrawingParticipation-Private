c19c3981239697b86c8450bd793044879f260132
Khoi Ngo <ngo.kho@northeastern.edu>
2025-03-17 11:49:55 -0400

    feat: update pagination UI

--- base/app/components/Tools/ToolTips.vue
+++ base/app/components/Tools/ToolTips.vue
@@ -120,7 +120,7 @@ const paginatedKeys = computed(() => {
   const end = start + itemsPerPage
   return keys.slice(start, end)
 })
-const totalLength = computed(() => {
+const total = computed(() => {
   return Object.keys(activePopup.value.content).length
 })
 
@@ -390,9 +390,36 @@ onUnmounted(() => {
       <div class="mt-2 !pointer-events-auto relative flex justify-center items-center pt-4">
         <UPagination
           v-model="page"
-          :total="totalLength"
-          :page-count="Math.ceil(totalLength / itemsPerPage)"
-        />
+          :total="total"
+          :page-count="itemsPerPage"
+          :ui="{ rounded: 'first-of-type:rounded-s-md last-of-type:rounded-e-md' }"
+        >
+          <template #prev="{ onClick, canGoPrev }">
+            <UTooltip text="Previous page">
+              <UButton
+                icon="i-heroicons-arrow-small-left-20-solid"
+                color="primary"
+                :ui="{ rounded: 'rounded-full' }"
+                class="rtl:[&_span:first-child]:rotate-180 me-2"
+                :disabled="!canGoPrev"
+                @click="onClick"
+              />
+            </UTooltip>
+          </template>
+
+          <template #next="{ onClick, canGoNext }">
+            <UTooltip text="Next page">
+              <UButton
+                icon="i-heroicons-arrow-small-right-20-solid"
+                color="primary"
+                :ui="{ rounded: 'rounded-full' }"
+                class="rtl:[&_span:last-child]:rotate-180 ms-2"
+                :disabled="!canGoNext"
+                @click="onClick"
+              />
+            </UTooltip>
+          </template>
+        </UPagination>
       </div>
     </div>
   </Teleport>
