27f7a888d041d64f9ae2cafb7eec2d2a0beaa31e
sevensrig <srinivasan.sri@northeastern.edu>
2025-06-07 17:40:26 -0400

    feat: Logout button added

--- apps/restart-ukraine/app/components/MenuModal.vue
+++ apps/restart-ukraine/app/components/MenuModal.vue
@@ -13,6 +13,16 @@
       >
         {{ item.label }}
       </UButton>
+      <UButton
+        block
+        color="red"
+        variant="solid"
+        icon="i-heroicons-arrow-right-on-rectangle"
+        class="dark:bg-red-700 text-white text-lg font-semibold rounded-full py-3 my-3"
+        @click="handleLogout"
+      >
+        Logout
+      </UButton>
     </UCard>
   </UModal>
 
@@ -23,9 +33,13 @@
 import { computed, ref } from 'vue'
 import { useRouter } from 'nuxt/app'
 import SupportModal from './SupportModal.vue'
+import { getAuth, signOut } from 'firebase/auth'
+import { useUserStore } from '@base/stores/user'
 
 const router = useRouter()
 const showSupportModal = ref(false)
+const auth = getAuth()
+const userStore = useUserStore()
 
 const props = defineProps({
   modelValue: {
@@ -92,4 +106,17 @@ const handleItemClick = (item) => {
 
   emit('select', item.action)
 }
+
+const handleLogout = async () => {
+  try {
+    await signOut(auth)
+    console.log('User logged out successfully')
+    closeModal()
+    userStore.logoutUser()
+    router.push('/')
+  } catch (error) {
+    console.error('Error signing out:', error)
+  }
+}
+
 </script>
