a5128e94ab80cb0d956eb65ad2e614852b891dff
sevensrig <srinivasan.sri@northeastern.edu>
2026-05-19 22:52:16 -0400

    feature filters

--- base/app/components/DrawingLayer/DrawingLayer.vue
+++ base/app/components/DrawingLayer/DrawingLayer.vue
@@ -26,7 +26,7 @@ import LineStringLayer from './LineStringLayer.vue'
 import PolygonLayer from './PolygonLayer.vue'
 import IconLayer from './IconLayer.vue'
 
-defineProps({
+const props = defineProps({
   projection: {
     type: String,
     required: true,
@@ -47,6 +47,10 @@ defineProps({
     type: Boolean,
     default: true,
   },
+  featureFilter: {
+    type: Function,
+    default: null,
+  },
 })
 
 const emit = defineEmits([
@@ -65,7 +69,8 @@ const currentColor = computed(() => sidebarStore.currentColor)
 
 const pointFeatures = computed(() => {
   const data = routeFeatureStore.getDataForRoute()
-  return data.filter(feature => feature.type === 'Point')
+  const points = data.filter(feature => feature.type === 'Point')
+  return props.featureFilter ? points.filter(props.featureFilter) : points
 },
 )
 
